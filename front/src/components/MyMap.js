f806283907cbecb4c5f14ab7c218928692a593d9
chaitanya1911 <kchaitanya1911@gmail.com>
2022-03-26 15:59:05 +0530

    addedMaps

---
+++ front/src/components/MyMap.js
@@ -0,0 +1,61 @@
+import { useLeafletContext } from "@react-leaflet/core";
+import L from "leaflet";
+import { useEffect } from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+function Square(props) {
+  const context = useLeafletContext();
+
+  useEffect(() => {
+    const bounds = L.latLng(props.center).toBounds(props.size);
+    const square = new L.Rectangle(bounds);
+    const container = context.layerContainer || context.map;
+    container.addLayer(square);
+
+    return () => {
+      container.removeLayer(square);
+    };
+  });
+
+  return null;
+}
+
+const center = [
+  [19.2307, 72.8567],
+  [19, 72],
+  [11.059821, 78.387451],
+  [17.123184, 79.208824],
+  [29.065773, 76.040497],
+  // [,],
+];
+const desc = [["maha"], ["del"], ["del"], ["del"], ["x"]];
+
+const myIcon = L.icon({
+  iconUrl: require("../images/marker-icon-2x.png"),
+  iconSize: [40, 50],
+});
+
+function MyMap() {
+  return (
+    <MapContainer
+      style={{ height: "400px", width: "500px" }}
+      center={[20.5937, 78.9629]}
+      zoom={4}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {center.map((mark, index) => {
+        return (
+          <Marker position={mark} icon={myIcon}>
+            <Popup>{desc[index]}</Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
+  );
+}
+export default MyMap;
